bdbc94ffc5f2c349487c28cfa00afaa9ccacad8d
blxzye <94803115+blxzye@users.noreply.github.com>
2022-08-19 19:07:55 -0300

    Add files via upload
    uploaded index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -2,14 +2,13 @@ const meta = document.getElementById("meta")
 const dificuldade= document.getElementById("dificuldade")
 const tempoN = document.getElementById("tempoN")
 const tempoM = document.getElementById("tempoM")
-const pontuation = document.getElementById("pontuation")
 
 function calculate(){
     if(meta.value!=""&&dificuldade.value!=""){
         const calculo = (60/((meta.value/dificuldade.value)/10))/100 
         tempoN.value = (5/100)*calculo + "segundos"
         tempoM.value = calculo + " segundos"
-        pontuation.value = meta.value * dificuldade.value + " pontos"
+        
     }
     if(dificuldade.value>5){
         dificuldade.value=5
