8a04e249dd270245bc4036392b039fc1313e0533
blxzye <94803115+blxzye@users.noreply.github.com>
2022-08-19 18:51:40 -0300

    Add files via upload
    added the files =)

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const meta = document.getElementById("meta")
+const dificuldade= document.getElementById("dificuldade")
+const tempoN = document.getElementById("tempoN")
+const tempoM = document.getElementById("tempoM")
+
+function calculate(){
+    if(meta.value!=""&&dificuldade.value!=""){
+        const calculo = (60/((meta.value/dificuldade.value)/10))/100 
+        tempoN.value = (5/100)*calculo + "segundos"
+        tempoM.value = calculo + " segundos"
+    }
+    if(dificuldade.value>5){
+        dificuldade.value=5
+        calculate()
+    }
+}
